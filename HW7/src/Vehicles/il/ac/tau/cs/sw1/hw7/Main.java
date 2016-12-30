3666e49278c80c2e8df703c67025fc5504b2eb8e
Jonathan <gescheit@mail.tau.ac.il>
2016-12-30 23:12:11 +0200

    Progress

--- HW7/src/Vehicles/il/ac/tau/cs/sw1/hw7/Main.java
+++ HW7/src/Vehicles/il/ac/tau/cs/sw1/hw7/Main.java
@@ -1,6 +1,11 @@
 package Vehicles.il.ac.tau.cs.sw1.hw7;
 
-import java.lang.reflect.Array;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -10,8 +15,16 @@ public class Main {
 	public static void main(String [] args)
 	{
 		Vehicle [] vehicles = getVehicleFromUser();
-//		writeVehiclesToFile(args[0], vehicles);
-//		writeVehiclesSummaryToFile(args[1], vehicles);
+		try {
+			writeVehiclesToFile(args[0], vehicles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			writeVehiclesSummaryToFile(args[1], vehicles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 
 	}
@@ -20,8 +33,9 @@ public class Main {
 	{
 		Vehicle[] vehicles = new Vehicle[MAX_VEHICLES];
 		int numOfVehicles = 0;
+		boolean exit = false;
 
-		while (numOfVehicles < MAX_VEHICLES) {
+		getTypes: while (numOfVehicles < MAX_VEHICLES) {
 			String type = getTypeFromUser();
 
 			switch (type) {
@@ -42,7 +56,7 @@ public class Main {
 					break;
 				case "X":
 					System.out.println("exit");
-					break;
+					break getTypes;
 				default:
 					System.out.println("Unknown command. Please try again.");
 					break;
@@ -53,14 +67,97 @@ public class Main {
 		return vehicles;
 	}
 	
-	public static void writeVehiclesToFile(String outputFilename, Vehicle[] vehicles) 
-	{
-		//TODO
+	public static void writeVehiclesToFile(String outputFilename, Vehicle[] vehicles) throws IOException {
+		File outputFile = new File(outputFilename);
+		outputFile.createNewFile();
+
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+
+		String landVehicles = "";
+		String seaVessels = "";
+
+		for (Vehicle vehicle : vehicles) {
+			if (vehicle == null) { break; }
+			String type = vehicle.getClass().getSimpleName();
+			switch (type) {
+				case "Jeep":
+					landVehicles += ((Jeep) vehicle).getDetails() + "\r\n";
+					break;
+				case "Boat":
+					seaVessels += ((Boat) vehicle).getDetails() + "\r\n";
+					break;
+				case "Hovercraft":
+					landVehicles += ((Hovercraft) vehicle).getDetails() + "\r\n";
+					seaVessels += ((Hovercraft) vehicle).getDetails() + "\r\n";
+					break;
+			}
+		}
+
+		bufferedWriter.write("Land vehicles:" + "\r\n\r\n");
+		bufferedWriter.write(landVehicles + "\r\n");
+		bufferedWriter.write("Sea vessels:" + "\r\n\r\n");
+		bufferedWriter.write(seaVessels);
+
+		bufferedWriter.close();
 	}
 	
-	public static void writeVehiclesSummaryToFile(String outputFilename, Vehicle[] vehicles) 
-	{
-		//TODO
+	public static void writeVehiclesSummaryToFile(String outputFilename, Vehicle[] vehicles) throws IOException {
+		File outputFile = new File(outputFilename);
+		outputFile.createNewFile();
+
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+
+		int totalLandVehicles = 0;
+		int totalLandPassengersPossible = 0;
+		ArrayList landSpeeds = new ArrayList();
+
+		int totalSeaVehicles = 0;
+		int totalSeaPassengersPossible = 0;
+		ArrayList seaSpeeds = new ArrayList();
+
+		for (Vehicle vehicle : vehicles) {
+			if (vehicle == null) { break; }
+			String type = vehicle.getClass().getSimpleName();
+			switch (type) {
+				case "Jeep":
+					totalLandVehicles++;
+					totalLandPassengersPossible += vehicle.getMaxPassengers();
+					landSpeeds.add(vehicle.getMaxSpeed());
+					break;
+				case "Boat":
+					totalSeaVehicles++;
+					totalSeaPassengersPossible += vehicle.getMaxPassengers();
+					seaSpeeds.add(vehicle.getMaxSpeed());
+					break;
+				case "Hovercraft":
+					totalLandVehicles++;
+					totalLandPassengersPossible += vehicle.getMaxPassengers();
+					landSpeeds.add(vehicle.getMaxSpeed());
+
+					totalSeaVehicles++;
+					totalSeaPassengersPossible += vehicle.getMaxPassengers();
+					seaSpeeds.add(vehicle.getMaxSpeed());
+					break;
+			}
+		}
+
+		Object landMaxSpeed = Collections.max(landSpeeds);
+		Object landMinSpeed = Collections.min(landSpeeds);
+		Object seaMaxSpeed = Collections.max(seaSpeeds);
+		Object seaMinSpeed = Collections.min(seaSpeeds);
+
+		bufferedWriter.write("Land vehicles:" + "\r\n\r\n");
+		bufferedWriter.write("Total land vehicles:" + totalLandVehicles + "\r\n");
+		bufferedWriter.write("Total passengers possible:" + totalLandPassengersPossible + "\r\n");
+		bufferedWriter.write("Max speed:" + landMaxSpeed + "\r\n");
+		bufferedWriter.write("Min speed:" + landMinSpeed + "\r\n\r\n");
+		bufferedWriter.write("Sea vessels:" + "\r\n\r\n");
+		bufferedWriter.write("Total sea vessels:" + totalSeaVehicles + "\r\n");
+		bufferedWriter.write("Total passengers possible:" + totalSeaPassengersPossible + "\r\n");
+		bufferedWriter.write("Max speed:" + seaMaxSpeed + "\r\n");
+		bufferedWriter.write("Min speed:" + seaMinSpeed + "\r\n");
+
+		bufferedWriter.close();
 	}
 
 	private static String getTypeFromUser() {
@@ -98,7 +195,7 @@ public class Main {
 	private static String promptUser(String message) {
 		System.out.println(message);
 		String input = scanner.nextLine();
-		if (input.isEmpty()) {
+		if (input.isEmpty() || !input.matches("[a-zA-Z0-9-_ ]+")) {
 			System.out.println("Illegal input. Please try again.");
 			promptUser(message);
 		}
